777037bea419eb92607314f6a3084927d04d0507
Alyona Yavorska <alyonayavorskaya03@gmail.com>
2020-07-25 20:44:58 +0300

    Application connected to MongoDB

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log("Test")
+const config = require("config");
+const express = require("express");
+const mongoose = require("mongoose")
+const app = express();
+const PORT = config.get('port') || 3000;
+
+async function start(){
+    try{
+        //Method 'conect' returns Promise, 'avait' while Promise will be completed 
+        mongoose.connect(config.get('mongoUri'),{
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        });
+        //Start server
+
+        app.listen(PORT, ()=>{
+            console.log(`Server is running on port - ${PORT}`);
+        }); 
+    }catch(e){
+        console.log("Server error "+e.message);
+        //Close Node process
+        process.exit(1);
+
+    }
+}
+start();
