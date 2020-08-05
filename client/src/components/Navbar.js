4d6b4f6991868f46eeb30606d781f5b78d3a43bf
Alyona Yavorska <alyonayavorskaya03@gmail.com>
2020-08-05 23:51:17 +0300

    Navbar was added

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,25 @@
+import React, {useContext} from 'react';
+import { AuthContext } from '../context/AuthContext';
+import { NavLink, useHistory } from 'react-router-dom';
+
+export const Navbar = ()=>{
+    const history = useHistory();
+    const auth = useContext(AuthContext);
+    const logoutHandler = (e)=>{
+        e.preventDefault();
+        auth.logout();
+        history.push("/");
+    };
+    return (
+        <nav>
+            <div className="nav-wrapper">
+            <span href="/" className="brand-logo">Shorten links</span>
+            <ul id="nav-mobile" className="right hide-on-med-and-down">
+                <li> <NavLink to="/create">Create</NavLink> </li>
+                <li> <NavLink to="/links">Links</NavLink> </li>
+                <li> <a href="/" onClick={logoutHandler}>logout</a> </li>
+            </ul>
+            </div>
+        </nav>
+   )
+}
