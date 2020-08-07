9db7c56d78b443231f35d1b557ed64455074d9ec
Alyona Yavorska <alyonayavorskaya03@gmail.com>
2020-08-07 14:47:14 +0300

    Middleware auth.middleware created for the router get('/generate') and also a shorten link and then it was saved into the DB

--- routers/link.routers.js
+++ routers/link.routers.js
@@ -1,12 +1,32 @@
 //Routers for link generation to make them shorten
 const {Router} = require("express");
 const Link = require("./../models/Link");
+const shortId = require("shortId")
 const auth = require("./../middlewares/auth.middleware");
+const config = require("config");
 const router = Router();
-
-router.post('/generate', async (request, response)=> {
+//'/api/link/generate'
+router.post('/generate', auth, async (request, response)=> {
     try{
-
+        const baseUrl = config.get("baseUrl");
+        const {from} = request.body;
+        //Create short ID qs a unique code
+        const code = shortId.generate();
+        const existing = await Link.findOne({ from });
+        //If link is already exists
+        if (existing) {
+            return response.json({ link: existing });
+        }
+        //Make shorten link for our service
+        const to = baseUrl + '/t/' + code;
+        //Create new link
+        const link = new Link({
+            code, to, from, owner: request.user.userId
+        });
+        //Save Link into the DB
+        await link.save();
+        //201 Created
+        response.status(201).json( {link} );
     }catch(e){
         response.status(500).json({message: "Something went wrong!"});
     }
