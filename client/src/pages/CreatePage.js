c9dcea377d01c0737762318978503950daec1451
Alyona Yavorska <alyonayavorskaya03@gmail.com>
2020-08-07 19:07:47 +0300

    Shorten link can be created and save into the DB

--- client/src/pages/CreatePage.js
+++ client/src/pages/CreatePage.js
@@ -1,7 +1,48 @@
-import React from 'react';
+import React, {useState, useEffect, useContext} from 'react';
+import {useHttp} from './../hooks/http.hook';
+import {AuthContext} from './../context/AuthContext';
+import {useHistory} from 'react-router-dom';
 
 export const CreatePage = ()=>{
+    const history = useHistory();
+    const [link,setLink] = useState('');
+    const { request } = useHttp();
+    const auth = useContext(AuthContext);
+    // Call as componentDidMount
+    useEffect( ()=>{
+        //Materialize clear inputs
+        window.M.updateTextFields();
+    },[]);
+    //Handle the Input when press Enter
+    const pressHandler = async (e)=>{
+        if(e.key === "Enter") {
+            try{
+                const data = await request(
+                  '/api/link/generate',
+                  'POST',
+                  {from: link},
+                  {Authorization: `AYAV ${auth.token}`}
+                );
+                history.push(`/detail/${data.link._id}`);
+                console.log(data);
+            }catch(e){}
+        }
+    }
     return (
-        <div>Create Page</div>
+        <div className="row">
+            <div className="col s8 offset-s2" style={ {paddingTop: '2rem'} }>
+                <div className="input-field">
+                    <input placeholder="link"
+                        id="link"
+                        type="text"
+                        value={link}
+                        onChange={ (event)=>{setLink(event.target.value);} }
+                        onKeyPress={pressHandler}
+                    />
+                    <label htmlFor="link">Create link</label>
+                </div>
+            </div>
+            <p>{link}</p>
+        </div>
     );
 }
--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const config = require("./../config/default.json");
+const config = require("config");
 
 //Middleware
 module.exports = (request, response, next)=>{
@@ -8,14 +8,14 @@ module.exports = (request, response, next)=>{
         return next();
     }
     try{
-        const token = request.headers.authorization.split(" ")[1];//"Bearer TOKEN"
+        const token = request.headers.authorization.split(" ")[1];//"AYAV TOKEN"
         if(!token){
             //401 Unauthorized, return - is to stop execution
             return response.status(401).json({message: "You aren't authorized"});
         }
         const decode = jwt.verify(token, config.get("jwtSecret"));
         //Create property user on object request and write decode token into it.
-        request.user - decode;
+        request.user = decode;
         //Go further
         next();
     }catch(e){
