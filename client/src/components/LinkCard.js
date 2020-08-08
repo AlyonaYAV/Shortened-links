d30da18d1d27232ffb06e502a261dcd9698e2eb4
Alyona Yavorska <alyonayavorskaya03@gmail.com>
2020-08-08 17:58:52 +0300

    LinkCard component has been created for link details on the DetailPage

---
+++ client/src/components/LinkCard.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export const LinkCard = ( {link} )=>{
+    return (
+        <>
+            <h2>This is a link</h2>
+            <p>It's your link:&nbsp;
+                <a href={link.to} target="_blank" rel="noopener noreferrer">
+                    {link.to}
+                </a>
+            </p>
+            <p>It's a link from:&nbsp;
+                <a href={link.from} target="_blank" rel="noopener noreferrer">
+                    {link.from}
+                </a>
+            </p>
+            <p>Amount of clicks:&nbsp;
+                <strong>{link.clicks}</strong>
+            </p>
+            <p>Creation date:
+                <strong>{ new Date(link.date).toLocaleDateString() }</strong>
+            </p>
+        </>
+    )
+};
