91acca643d101d9fac4b61fc1b170c8dec5d6168
Alyona Yavorska <alyonayavorskaya03@gmail.com>
2020-08-08 18:02:01 +0300

    Link info displays correctly on the page DetailPage

--- client/src/pages/DetailPage.js
+++ client/src/pages/DetailPage.js
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { useCallback, useContext, useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { useHttp } from '../hooks/http.hook';
+import { AuthContext } from '../context/AuthContext';
+import { Loader } from '../components/Loader';
+import { LinkCard } from '../components/LinkCard';
 
 export const DetailPage = ()=>{
+    const { token } = useContext(AuthContext);
+    const linkId = useParams().id;
+    const { request, loading } = useHttp();
+    const [link, setLink] = useState(null);
+    const getLink = useCallback( async ()=>{
+        try{
+            const fetched = await request(`/api/link/${linkId}`, 'GET', null, {
+                Authorization: `AYAV ${token}`
+            });
+            setLink(fetched);
+        }catch(error){
+
+        }
+    }, [token, linkId, request]);
+
+    useEffect( ()=>{
+        getLink();
+
+    }, [getLink]);
+    // While is loading display Loader
+    if (loading){
+        return <Loader />;
+    }
     return (
-        <div>Detail Page</div>
+        <>
+            { !loading && link && <LinkCard link={ link } />}
+        </>
     );
 }
