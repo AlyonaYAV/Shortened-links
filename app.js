1f8cb64ccdebccdc0c810fdea1039a901c07e5bc
Alyona Yavorska <alyonayavorskaya03@gmail.com>
2020-08-09 17:32:43 +0300

    Prepare for production

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const config = require("config");
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+const path = require("path");
+const { request } = require("http");
+const { response } = require("express");
 const PORT = config.get('port') || 3000;
 
 //To get correct property 'body' from Request object. 'body' is a stream.
@@ -11,6 +14,14 @@ app.use(express.json({ extended:true }));
 app.use('/api/auth', require('./routers/auth.routers'));
 app.use('/api/link', require('./routers/link.routers'));
 app.use('/t', require('./routers/redirect.routers'));
+
+if(process.env.NODE_ENV === 'production'){
+    app.use('/', express.static(path.join(__dirname, 'client', 'build')));
+
+    app.get('*',(request, response)=>{
+        response.sentFile( path.resolve(__dirname, 'client', 'build', 'index.html'))
+    };
+}
 
 async function start(){
     try{
